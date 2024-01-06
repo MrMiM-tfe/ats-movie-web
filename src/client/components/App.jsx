8f33d44353b13a7ea8053682013e586930b9ec63
MrMiM <mahdikhakbaz7@gmail.com>
2024-01-06 23:20:49 +0330

    working

---
+++ src/client/components/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+       <div>
+        hello my name is mahdi
+       </div>
+    );
+};
+
+export default App;
---
+++ src/client/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {hydrateRoot} from 'react-dom/client';
+import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+// import {ThemeProvider} from '@mui/material/styles';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import christmasTheme from './themes/christmasTheme';
+// import {CacheProvider} from '@emotion/react';
+// import createEmotionCache from './createEmotionCache';
+
+
+hydrateRoot(document.getElementById('root'),
+  <React.StrictMode>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={christmasTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </CacheProvider>
+  </React.StrictMode>
+);
